b0683e708bc6d07dba8b03732c178756ac8d6551
Papiya Halder <papiyahalder737@gmail.com>
2021-05-14 21:32:14 +0530

    fronten fetured product

--- laravel8eommerce/app/Http/Controllers/FrontendController.php
+++ laravel8eommerce/app/Http/Controllers/FrontendController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 
 class FrontendController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $products = Product::Where('status' ,'1')->get();
+        $categories = Category::Where('status' ,'1')->get();
         // return $products;
-        return view('pages.index', compact('products'));
+        // $featuresproducts = Product::Where('category_id','$categories->id')
+        // ->Where('status' ,'1')->get();
+        //  return $featuresproducts;
+        return view('pages.index', compact('products','categories'));
     }
-}
+} 
